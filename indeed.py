63b4d2ed210ee77d454c98454bce8f4517d18f80
kimgeuntae <kok617@naver.com>
2019-11-25 16:38:21 +0900

    Add Get [title, company_name]

--- indeed.py
+++ indeed.py
@@ -1,10 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-INDEED_URL = "https://www.indeed.com/jobs?as_and=python&limit=50"
+LIMIT = 50
+URL = f"https://www.indeed.com/jobs?as_and=python&limit={LIMIT}"
 
 def extract_indeed_pages():
-    result = requests.get(INDEED_URL)
+    result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
 
     pagination = soup.find("div", {"class": "pagination"})
@@ -17,3 +18,23 @@ def extract_indeed_pages():
 
     max_page = pages[-1]
     return max_page
+
+def extract_indeed_jobs(last_page):
+    jobs = []
+    #for page in range(last_page):
+    result = requests.get(f"{URL}&start={0*LIMIT}")
+    soup = BeautifulSoup(result.text, "html.parser")
+    results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+    for result in results:
+        title = result.find("div", {"class": "title"}).find("a")["title"]
+        company = result.find("span", {"class": "company"})
+        conpany_anchor = company.find("a")
+        if conpany_anchor is not None:
+            company = conpany_anchor.string
+        else:
+            company = company.string
+        company = company.strip()
+        print(title, company)
+    return jobs
+
+
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from indeed import extract_indeed_pages
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
+last_indeed_page = extract_indeed_pages()
 
-max_indeed_pages = extract_indeed_pages()
-
-print(max_indeed_pages)
-
+indeed_jobs = extract_indeed_jobs(last_indeed_page)
