f272eb925973d5a6ea0e3eb7a4904bd8ea352909
kimgeuntae <kok617@naver.com>
2019-11-25 21:07:09 +0900

    Done scraped

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from indeed import get_jobs as get_indeed_jobs
 from stackover import get_jobs as get_so_jobs
+from save import save_to_file
 
 stackover_jobs = get_so_jobs()
 indeed_jobs = get_indeed_jobs()
 
 jobs = stackover_jobs + indeed_jobs
-print(jobs)
+
+save_to_file(jobs)
