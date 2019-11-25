42bc2aa478e956984a3bc4778b51370981d11db8
kimgeuntae <kok617@naver.com>
2019-11-25 14:22:08 +0900

    Add module [BeautifulSoup]

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
-#from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
 indeed_result = requests.get("https://www.indeed.com/jobs?as_and=python&limit=50")
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
-print(indeed_result)
+print(indeed_soup)
+
+
